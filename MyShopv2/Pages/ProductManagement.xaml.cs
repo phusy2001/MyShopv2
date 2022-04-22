f0ef77f813669fdedcbe1b80c20c0196941f40ed
Nguyễn Ngọc Phú Sỹ <phusy9a11516@gmail.com>
2022-04-22 21:08:06 +0700

    Demo Data Binding Listview

--- MyShopv2/Pages/ProductManagement.xaml.cs
+++ MyShopv2/Pages/ProductManagement.xaml.cs
@@ -1,17 +1,10 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using MyShop.Data;
+using MyShop.Models;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MyShopv2.Pages
 {
@@ -20,9 +13,42 @@ namespace MyShopv2.Pages
     /// </summary>
     public partial class ProductManagement : Page
     {
+        private readonly ApplicationDbContext _context = new ApplicationDbContext();
+        private CollectionViewSource ProductViewSource;
+
         public ProductManagement()
         {
             InitializeComponent();
+            ProductViewSource = (CollectionViewSource)FindResource(nameof(ProductViewSource));
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var products = new List<Product>()
+            {
+                new Product()
+                {
+                    Id = 1,
+                    Name = "San pham thu 1",
+                    Description = "Mo ta san pham",
+                    Price = 2000000,
+                    imageURL = "No image",
+                    Quantity = 5,
+                    CategoryID = 1,
+                    CreatedAt = "22/04/2022",
+                    UpdatedAt = "22/04/2022"
+                }
+            };
+            // this is for demo purposes only, to make it easier
+            // to get up and running
+            _context.Database.EnsureCreated();
+
+            // load the entities into EF Core
+            _context.Products.Load();
+
+            // bind to the source
+            ProductViewSource.Source = _context.Products.Local.ToObservableCollection();
+        }
+
     }
 }
